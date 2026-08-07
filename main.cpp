d20b5af371fbe01f2c982577de94311013d01b25
Jake Boraman <jake.boraman@gmail.com>
2026-08-07 17:04:20 +0100

    Added empty ImGui UI

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include "banking_testing.h"
+#include "imgui.h"
+#include "GuiManager.h"
+#include <iostream>
+
 int main() {
+	// Initialise UI window
+	if (!GuiManager::Init("Banking System", 400, 300)) {
+		return -1;
+	}
+
+	std::cout << "Running UI..." << std::endl;
+
+	// UI loop
+	while (GuiManager::IsRunning()) {
+		GuiManager::BeginFrame();
+
+
+		GuiManager::EndFrame();
+	}
+
+	// Run bank stuff
 	get_going();
+
+	GuiManager::Shutdown();
 	return 0;
 }
