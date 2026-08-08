aef8140a9018adb8d51ce365f698d8ab8a082b88
Jake Boraman <jake.boraman@gmail.com>
2026-08-08 14:13:40 +0100

    Basic UI tabs

--- main.cpp
+++ main.cpp
@@ -15,16 +15,48 @@ int main() {
 	// UI loop
 	while (GuiManager::IsRunning()) {
 		GuiManager::BeginFrame();
+		ImGui::SetNextWindowPos(ImVec2(0, 0));
+		ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
 
-		// BANK SYSTEM TESTS
-		// Only run long tests if checkbox checked
-		static bool run_long = false;
-		ImGui::Checkbox("Run Long Tests", &run_long);
+		ImGuiWindowFlags windowFlags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse;
 
-		if (ImGui::Button("Run Bank Tests")) {
-			run_bank_tests(run_long);
+		ImGui::Begin("MainLayout", nullptr, windowFlags);
+
+		if (ImGui::BeginTabBar("MainNavigationTabBar")) {
+			// ------------- BANK SYSTEM TESTS --------------------------------
+			if (ImGui::BeginTabItem("Bank System Tests")) {
+				static bool run_long = false;
+
+				if (ImGui::Button("Run Bank Tests")) {
+					run_bank_tests(run_long);
+				}
+
+				ImGui::SameLine();
+				// Only run long tests if checkbox checked
+				ImGui::Checkbox("Run Long Tests", &run_long);
+
+				ImGui::EndTabItem();
+			}
+			// ----------------------------------------------------------------
+
+			// ------------- USER AREA ----------------------------------------
+			if (ImGui::BeginTabItem("User")) {
+
+				ImGui::EndTabItem();
+			}
+			// ----------------------------------------------------------------
+
+			// ------------- ADMIN AREA ---------------------------------------
+			if (ImGui::BeginTabItem("Admin")) {
+
+				ImGui::EndTabItem();
+			}
+			// ----------------------------------------------------------------
+
+			ImGui::EndTabBar();
 		}
 
+		ImGui::End();
 		GuiManager::EndFrame();
 	}
 
