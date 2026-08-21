34a009e4e511e0d30c8e88a09a1ae09567d8cac2
Jake Boraman <jake.boraman@gmail.com>
2026-08-21 13:31:47 +0100

    More init output

--- main.cpp
+++ main.cpp
@@ -14,10 +14,11 @@ int main() {
 		std::cerr << "Failed to initialise bank tests.\n";
 		return EXIT_FAILURE;
 	}
+	std::cout << "Tests successfully initialised..." << std::endl;
 	
 	// Init database
 	SQLite::Database db = database::open_database("bank.db");
-	std::cout << "SQLiteCpp initialised successfully!" << std::endl;
+	std::cout << "Database successfully initialised..." << std::endl;
 	std::cout << "SQLite C Version: " << SQLite::getLibVersion() << std::endl;
 	std::cout << "SQLiteCpp Wrapper Version: " << SQLITECPP_VERSION << std::endl;
 	if (!database::init_tables(db)) {
@@ -27,12 +28,14 @@ int main() {
 
 	// Init bank
 	bank_system::Bank bank(db);
+	std::cout << "Bank system successfully initialised..." << std::endl;
 
 	// Init UI
 	if (!ui::init_ui("Banking System", 750, 350)) {
 		std::cerr << "Failed to initialise UI system.\n";
 		return EXIT_FAILURE;
 	}
+	std::cout << "UI successfully initialised..." << std::endl;
 
 	std::cout << "Running UI..." << std::endl;
 
